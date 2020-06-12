f882896ad841cd7f0f17c27fdb8528e6f516b1cb
NiklasReents <Niklas.Reents@gmail.com>
2020-06-12 12:33:56 +0200

    add button functions for previous, next and random meme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-const fetchFunction = (setData, memeAPI) => {
+const fetchFunction = (setMemes, memeAPI) => {
   fetch(memeAPI)
     .then((result) => result.json())
     .then((finalResult) => {
-      setData(finalResult.data.memes[9]);
+      setMemes(finalResult.data.memes);
     })
     .catch((error) => console.log(error));
 };
 
 function App() {
-  const [data, setData] = useState("");
+  const [memes, setMemes] = useState([]);
+  const [currentMeme, setCurrentMeme] = useState(0);
+  const [topPictureText, setTopPictureText] = useState("");
+  const [bottomPictureText, setBottomPictureText] = useState("");
   const memeAPI = "https://api.imgflip.com/get_memes";
-  console.log(memeAPI);
 
   useEffect(() => {
-    fetchFunction(setData, memeAPI);
+    fetchFunction(setMemes, memeAPI);
   }, []);
 
   return (
@@ -24,22 +26,81 @@ function App() {
       <h1>Create Some Dank Memes!</h1>
       <div id="formWrapper">
         <span>
-          <form>
-            <input type="text" placeholder="top caption" />
-          </form>
+          {/*onChange={(event) => {
+              setTopPictureText(event.target.value);
+            }}*/}
+          <input id="toptext" type="text" placeholder="top caption" />
         </span>
         <span>
-          <form>
-            <input type="text" placeholder="bottom caption" />
-          </form>
+          {/*onChange={(event) => {
+              setBottomPictureText(event.target.value);
+            }}*/}
+          <input id="bottomtext" type="text" placeholder="bottom caption" />
         </span>
+        <button
+          onClick={() => {
+            const firstvalue = document.getElementById("toptext").value;
+            setTopPictureText(firstvalue);
+            const secondvalue = document.getElementById("bottomtext").value;
+            setBottomPictureText(secondvalue);
+          }}
+        >
+          Create Meme!
+        </button>
       </div>
       <div>
-        <button>Previous Picture</button>
-        <button>Next Picture</button>
-        <button type="button">Generate Random Meme</button>
+        <button
+          onClick={() => {
+            const previousMeme = currentMeme;
+            if (previousMeme > 0) setCurrentMeme(previousMeme - 1);
+            else setCurrentMeme(memes.length - 1);
+          }}
+          type="button"
+        >
+          Previous Picture
+        </button>
+        <button
+          onClick={() => {
+            const nextMeme = currentMeme;
+            if (nextMeme < memes.length - 1) setCurrentMeme(nextMeme + 1);
+            else {
+              setCurrentMeme(0);
+            }
+          }}
+          type="button"
+        >
+          Next Picture
+        </button>
+        <button
+          onClick={() => {
+            setCurrentMeme([Math.floor(Math.random() * memes.length)]);
+          }}
+          type="button"
+        >
+          Generate Random Meme
+        </button>
       </div>
-      <div>{data ? <img src={data.url} alt="meme" /> : null}</div>
+      <div>
+        <div className="container">
+          <div className="text-block">
+            <h1>{topPictureText}</h1>
+          </div>
+          <div>
+            {memes[currentMeme] ? (
+              <img
+                src={memes[currentMeme].url}
+                alt="Meme"
+                style={{ width: "100%" }}
+              />
+            ) : (
+              []
+            )}
+          </div>
+          <div className="text-block-2">
+            <h1>{bottomPictureText}</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
